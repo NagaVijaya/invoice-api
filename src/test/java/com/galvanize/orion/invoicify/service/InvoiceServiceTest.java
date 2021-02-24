73a9ddb2040b79a2befb853ac40d25da321dee5e
tkrishnak <tkrishnagokul@gmail.com>
2021-02-23 19:35:12 -0500

    Added unit test for invoice with multiple line item for invoice service

--- src/test/java/com/galvanize/orion/invoicify/service/InvoiceServiceTest.java
+++ src/test/java/com/galvanize/orion/invoicify/service/InvoiceServiceTest.java
@@ -1,6 +1,7 @@
 package com.galvanize.orion.invoicify.service;
 
 import com.galvanize.orion.invoicify.entities.Invoice;
+import com.galvanize.orion.invoicify.entities.LineItem;
 import com.galvanize.orion.invoicify.repository.InvoiceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -34,6 +36,29 @@ public class InvoiceServiceTest {
         assertEquals(expectedInvoice.getCompany(), actualInvoice.getCompany());
         assertEquals(expectedInvoice.getTotalCost(), actualInvoice.getTotalCost());
         assertEquals(expectedInvoice.getCreatedDate(), actualInvoice.getCreatedDate());
+        verify(invoiceRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void testCreateInvoiceMultipleLineIem(){
+        LineItem lineItem = LineItem.builder().description("project 1").quantity(10).rate(5.4).build();
+        LineItem lineItem2 = LineItem.builder().description("project 2").quantity(10).rate(4.6).build();
+        List<LineItem> lineItemList = new ArrayList<>();
+        lineItemList.add(lineItem);
+        lineItemList.add(lineItem2);
+        Invoice invoice = Invoice.builder().author("Gokul").company("Cognizant").lineItem(lineItemList).build();
+        Invoice expectedInvoice = Invoice.builder().id(1L).author("Gokul").company("Cognizant").lineItem(lineItemList).totalCost(100).createdDate(new Date()).build();
+        InvoiceService invoiceService = new InvoiceService(invoiceRepository);
+        when(invoiceRepository.save(any())).thenReturn(expectedInvoice);
+        Invoice actualInvoice = invoiceService.createInvoice(invoice);
+        assertEquals(expectedInvoice.getId(), actualInvoice.getId());
+        assertEquals(expectedInvoice.getAuthor(), actualInvoice.getAuthor());
+        assertEquals(expectedInvoice.getCompany(), actualInvoice.getCompany());
+        assertEquals(expectedInvoice.getTotalCost(), actualInvoice.getTotalCost());
+        assertEquals(expectedInvoice.getCreatedDate(), actualInvoice.getCreatedDate());
+        assertEquals(expectedInvoice.getLineItem().size(), 2);
+        assertEquals(expectedInvoice.getLineItem().get(0).getFee(), 54);
+        assertEquals(expectedInvoice.getLineItem().get(1).getFee(), 46);
         verify(invoiceRepository, times(1)).save(any());
     }
 }
