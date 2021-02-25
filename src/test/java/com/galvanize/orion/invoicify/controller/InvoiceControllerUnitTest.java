888cd8e1869c3a37e0b52f1b87e4da650a0e4aa5
NagaVijaya <nagavijayapatibandla@gmail.com>
2021-02-24 19:00:25 -0500

    merged Controller unit test files

--- src/test/java/com/galvanize/orion/invoicify/controller/InvoiceControllerUnitTest.java
+++ src/test/java/com/galvanize/orion/invoicify/controller/InvoiceControllerUnitTest.java
@@ -1,24 +1,29 @@
 package com.galvanize.orion.invoicify.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.orion.invoicify.entities.Invoice;
 import com.galvanize.orion.invoicify.service.InvoiceService;
 import org.apache.commons.lang3.builder.ToStringExclude;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = InvoiceController.class)
 public class InvoiceControllerUnitTest {
 
     @Autowired
@@ -27,6 +32,17 @@ public class InvoiceControllerUnitTest {
     @MockBean
     InvoiceService invoiceService;
 
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    public void createInvoiceCallsInvoiceService() throws Exception {
+        Invoice invoice = Invoice.builder().author("Gokul").company("Cognizant").lineItem(new ArrayList<>()).build();
+        when(invoiceService.createInvoice(any())).thenReturn(invoice);
+        mockMvc.perform(post("/api/v1/invoice").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(invoice)));
+        verify(invoiceService, times(1)).createInvoice(any());
+    }
+
     @Test
     public void test_getAllInvoicesEndPoint_returnsOk() throws Exception {
         mockMvc.perform(get("/api/v1/invoices"))
@@ -34,14 +50,14 @@ public class InvoiceControllerUnitTest {
     }
 
     @Test
-    public void  test_getAllInvoices_returnsObject() throws Exception {
+    public void test_getAllInvoices_returnsObject() throws Exception {
         mockMvc.perform(get("/api/v1/invoices"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").exists());
     }
 
     @Test
-    public void  test_getAllInvoices_returns_emptyList() throws Exception {
+    public void test_getAllInvoices_returns_emptyList() throws Exception {
         List<String> stringList = new ArrayList<>();
         when(invoiceService.getAllInvoices()).thenReturn(stringList);
         mockMvc.perform(get("/api/v1/invoices"))
@@ -51,7 +67,7 @@ public class InvoiceControllerUnitTest {
     }
 
     @Test
-    public void  test_getAllInvoices_returns_singleInvoice() throws Exception {
+    public void test_getAllInvoices_returns_singleInvoice() throws Exception {
         List<String> stringList = new ArrayList<>();
         stringList.add("invoice1");
         when(invoiceService.getAllInvoices()).thenReturn(stringList);
