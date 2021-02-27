1db6300acf925d22d28bf9963e112042871cbce8
srijan4friends <srijan4friends@gmail.com>
2021-02-27 15:15:37 -0600

    merged with master changes

--- src/test/java/com/galvanize/orion/invoicify/controller/InvoiceControllerUnitTest.java
+++ src/test/java/com/galvanize/orion/invoicify/controller/InvoiceControllerUnitTest.java
@@ -196,7 +196,6 @@ public class InvoiceControllerUnitTest {
 
     @Test
     public void test_delete_invoice() throws Exception {
-
         mockMvc.perform(delete("/api/v1/invoice/4fa30ded-c47c-436a-9616-7e3b36be84b2"))
                 .andExpect(status().isOk());
 
